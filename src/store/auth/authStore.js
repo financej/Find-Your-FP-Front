c2176c12b5d68adbfbac612a1c090d22f308b5ff
Paperkeem <ahharlawhddl@gmail.com>
2024-03-22 13:16:09 +0900

    fix: persist 관련 fix

--- src/store/auth/authStore.js
+++ src/store/auth/authStore.js
@@ -1,17 +1,14 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-export const useAuthStore = create(
-  persist((set) => ({
-    token: "", //토큰
-    userName: "", //사용자 이름
-    isLogin: false, // 사용자 인증 상태
-    login: () =>
-      set({
-        isLogin: true,
-        token: JSON.parse(localStorage.getItem("accessToken")) ?? "",
-        userName: JSON.parse(localStorage.getItem("userName")) ?? "",
-      }), // 로그인 함수
-    logout: () => set({ isLogin: false, token: "" }), // 로그아웃 함수
-  }))
-);
+export const useAuthStore = create((set) => ({
+  token: "", //토큰
+  userName: "", //사용자 이름
+  isLogin: false, // 사용자 인증 상태
+  login: () =>
+    set({
+      isLogin: true,
+      token: JSON.parse(localStorage.getItem("accessToken")) ?? "",
+      userName: JSON.parse(localStorage.getItem("userName")) ?? "",
+    }), // 로그인 함수
+  logout: () => set({ isLogin: false, token: "" }), // 로그아웃 함수
+}));
