3108b766a9e46b92ad50991348426717bd952f2e
ByeongWoo Lee <qoddn987@gmail.com>
2024-03-23 00:37:20 +0900

    feat: SideBar 크기 작아지면 없어지도록 설정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
       </header>
       <section className="pt-16">
         <main className="flex">
-          <nav className="basis-2/12">
+          <nav className="hidden lg:block lg:basis-2/12">
             <SideBar />
           </nav>
           <main className="flex flex-col w-full gap-6 p-10 mx-auto basis-10/12">
