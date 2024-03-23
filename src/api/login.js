5258e491d61e2b88a70af9aba671b8da4f541661
Paperkeem <ahharlawhddl@gmail.com>
2024-03-24 01:59:29 +0900

    feat: 로그인 주소 변경

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 import { api } from "./instance";
 
 export const loginApi = async (code) => {
-  const response = await api.post(`/login/kakao`, {
+  const response = await api.post(`auth/login/kakao`, {
     code,
     redirectUri: import.meta.env.VITE_REDIRECT_URL,
   });
