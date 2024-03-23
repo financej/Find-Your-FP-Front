b653d2d5e532e66a2ff124199a173ecebcee2e12
Paperkeem <ahharlawhddl@gmail.com>
2024-03-24 02:42:24 +0900

    fix: style

--- src/components/layout/header/nav/NavigationBar.jsx
+++ src/components/layout/header/nav/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import { useAuthStore } from "@/store";
 // import { useAuthStore, useConfirmModalStore } from "@/store";
 // import { SearchIcon } from "@/ui/icons";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import {
   Avatar,
@@ -13,25 +13,25 @@ import {
 } from "@mui/material";
 // import { Link } from "react-router-dom";
 import SearchBar from "../../searchBar/SearchBar";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const CustomTypography = styled(Typography)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+  display: "flex",
+  alignItems: "center",
   gap: theme.spacing(2), // 테마 스페이싱 사용
-  fontSize: '14px'
+  fontSize: "14px",
   // 추가할 다른 스타일
 }));
 
-const StyledDiv = styled('div')(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  '&.hover': {
-    cursor: 'pointer',
-    background: 'red'
-  }
+const StyledDiv = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  "&.hover": {
+    cursor: "pointer",
+    background: "red",
+  },
 }));
 
 const KAKAO_REST_API = import.meta.env.VITE_KAKAO_API;
@@ -43,7 +43,7 @@ const NavigationBar = () => {
   const { isLogin, logout, userName } = useAuthStore();
   const theme = useTheme();
   const navigate = useNavigate();
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   // const handleLogin = () => {
   //   console.log("emf");
   //   openConfirmModal({ message: "되니?" }).then(
@@ -61,7 +61,7 @@ const NavigationBar = () => {
   return (
     <nav className="">
       <ul className="flex items-center gap-4 md:gap-7">
-        {pathname === "/search" && (
+        {pathname === "/search" ? (
           <Box
             sx={{
               width: 800,
@@ -70,44 +70,41 @@ const NavigationBar = () => {
           >
             <SearchBar />
           </Box>
+        ) : (
+          <>
+            <StyledDiv>
+              <CustomTypography onClick={() => navigate("/searchMain")}>
+                보험설계사 찾기
+              </CustomTypography>
+            </StyledDiv>
+            <StyledDiv>
+              <CustomTypography>상품 및 혜택</CustomTypography>
+              <ExpandLess />
+            </StyledDiv>
+            <StyledDiv>
+              <CustomTypography>고객지원</CustomTypography>
+              <ExpandLess />
+            </StyledDiv>
+            <StyledDiv>
+              <CustomTypography>회사 소개</CustomTypography>
+              <ExpandLess />
+            </StyledDiv>
+          </>
         )}
-        <StyledDiv>
-          <CustomTypography onClick={() => navigate('/searchMain')}>
-            보험설계사 찾기
-          </CustomTypography>
-        </StyledDiv>
-        <StyledDiv>
-          <CustomTypography>
-            상품 및 혜택
-          </CustomTypography>
-          <ExpandLess />
-        </StyledDiv>
-        <StyledDiv>
-          <CustomTypography>
-            고객지원
-          </CustomTypography>
-          <ExpandLess />
-        </StyledDiv>
-        <StyledDiv>
-          <CustomTypography>
-            회사 소개
-          </CustomTypography>
-          <ExpandLess />
-        </StyledDiv>
 
         <div>
           {!isLogin ? (
-              <>
-                <Button
-                    variant="outlined"
-                    onClick={handleKaKaoLogin}
-                    sx={{
-                      borderRadius: "50px",
-                    }}
-                >
-                  <Typography variant="body1">로그인</Typography>
-                </Button>
-              </>
+            <>
+              <Button
+                variant="outlined"
+                onClick={handleKaKaoLogin}
+                sx={{
+                  borderRadius: "50px",
+                }}
+              >
+                <Typography variant="body1">로그인</Typography>
+              </Button>
+            </>
           ) : (
             <Stack direction="row" spacing={1}>
               {/* <Avatar sx={{ bgcolor: theme.palette.primary.main }}>
--- src/screens/protected/search/HomePage.jsx
+++ src/screens/protected/search/HomePage.jsx
@@ -2,107 +2,99 @@ import { Box, Typography } from "@mui/material";
 import MainImageUp from "../../../../public/asset/mainimg1.svg";
 import MainImageDown from "../../../../public/asset/mainimg2.svg";
 import MainImageLeft from "../../../../public/asset/mainimg3.svg";
-import {styled} from "@mui/material/styles";
-import {useNavigate} from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 const CustomTypography = styled(Typography)(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: theme.spacing(2),
-    fontSize: '35px',
-    marginBottom: '50px'
+  display: "flex",
+  alignItems: "center",
+  gap: theme.spacing(2),
+  fontSize: "35px",
+  marginBottom: "50px",
 }));
 
-const CustomButton = styled('button')({
-    fontSize: '14px',
-    marginRight: '5px',
-    width: '312px',
-    height: '48px',
-    background: 'yellow'
+const CustomButton = styled("button")({
+  fontSize: "14px",
+  marginRight: "5px",
+  width: "312px",
+  height: "48px",
+  background: "yellow",
 });
 
 const CustomTypographyExample = styled(Typography)(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: theme.spacing(2),
-    fontSize: '14px',
-    color: '4B4B4B',
-    marginBottom: '20px'
+  display: "flex",
+  alignItems: "center",
+  gap: theme.spacing(2),
+  fontSize: "14px",
+  color: "4B4B4B",
+  marginBottom: "20px",
 }));
 
-const LeftDiv = styled('div')({
-    height: "calc(100vh - 64px)",
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
+const LeftDiv = styled("div")({
+  height: "calc(100vh - 64px)",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
 });
 
-const RightDiv = styled('div')({
-    display: 'flex',
-    width: '300px',
-    height: "calc(100vh - 64px)",
-    flexDirection: 'column',
-    justifyContent: 'center'
+const RightDiv = styled("div")({
+  display: "flex",
+  width: "300px",
+  height: "calc(100vh - 64px)",
+  flexDirection: "column",
+  justifyContent: "center",
 });
 
-const LeftImage = styled('img')({
-    width: '340px',
-    height: '230px',
-    margin: '5px',
-    display: 'flex',
-    position: 'static'
+const LeftImage = styled("img")({
+  width: "340px",
+  height: "230px",
+  margin: "5px",
+  display: "flex",
+  position: "static",
 });
 
-const RightImage = styled('img')({
-    width: '450px',
-    height: '470px',
-    margin: '5px',
+const RightImage = styled("img")({
+  width: "450px",
+  height: "470px",
+  margin: "5px",
 });
 
 export default function HomePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-      <Box
-          sx={{
-              margin: "0",
-              height: "calc(100vh - 64px)",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItem: "center",
-              flexWrap: "wrap",
-          }}
-      >
-          <LeftDiv>
-              <LeftImage
-                  src={MainImageUp}
-              />
-              <LeftImage
-                  src={MainImageDown}
-              />
-          </LeftDiv>
-          <LeftDiv>
-              <RightImage
-                  src={MainImageLeft}
-              />
-          </LeftDiv>
-          <RightDiv>
-              <CustomTypography>
-                  이제 내 주변의 <br/>
-                  보험 전문가를 <br/>
-                  집에서 <br/>
-                  만나보세요.
-              </CustomTypography>
-              <CustomTypographyExample>
-                  행복한 내일은 행복한 오늘이 모여
-                  만들어지기에 오늘부터,
-                  메트라이프
-              </CustomTypographyExample>
-              <CustomButton onClick={() => navigate('/login')}>
-                    내 주위 보험설계사 찾기
-              </CustomButton>
-          </RightDiv>
-      </Box>
+    <Box
+      sx={{
+        margin: "0",
+        height: "calc(100vh - 64px)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItem: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <LeftDiv>
+        <LeftImage src={MainImageUp} />
+        <LeftImage src={MainImageDown} />
+      </LeftDiv>
+      <LeftDiv>
+        <RightImage src={MainImageLeft} />
+      </LeftDiv>
+      <RightDiv>
+        <CustomTypography>
+          이제 내 주변의 <br />
+          보험 전문가를 <br />
+          집에서 <br />
+          만나보세요.
+        </CustomTypography>
+        <CustomTypographyExample>
+          행복한 내일은 행복한 오늘이 모여 만들어지기에 오늘부터, 메트라이프
+        </CustomTypographyExample>
+        <CustomButton onClick={() => navigate("/login")}>
+          내 주위 보험설계사 찾기
+        </CustomButton>
+      </RightDiv>
+    </Box>
   );
 }
