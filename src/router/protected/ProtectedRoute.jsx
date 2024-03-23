291789f7ddf73f935bbd2fe5fdfdc70a9dbb8df0
Paperkeem <ahharlawhddl@gmail.com>
2024-03-23 20:07:05 +0900

    update: 로그인 안했을 시 카카오 로그인 페이지로 redirect

--- src/router/protected/ProtectedRoute.jsx
+++ src/router/protected/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
-import { useAuthStore } from '@/store'
-import { Outlet, Navigate } from 'react-router-dom'
+import { useAuthStore } from "@/store";
+import { Outlet, Navigate } from "react-router-dom";
+import { KAKAO_URL } from "../../components/layout/header/nav/NavigationBar";
 
 export default function ProtectedRoute() {
+  const { token, isLogin } = useAuthStore();
 
-    const { token, isLogin } = useAuthStore()
+  if (!token || !isLogin) {
+    window.location.href = KAKAO_URL;
+    return null;
+  }
 
-    return token && isLogin ? <Outlet /> : <Navigate to="/login" replace />;
+  return token && isLogin ? <Outlet /> : <Navigate to={"/"} replace />;
 }
